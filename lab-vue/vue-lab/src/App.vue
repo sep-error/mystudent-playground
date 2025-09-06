4f014c366f80639f2928544286cb5a1b827e152f
edwardddd1 <edwardjohndomingo1@gmail.com>
2025-09-06 09:39:10 +0800

    Add Vue lab with StudentCard component

---
+++ lab-vue/vue-lab/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>My Classmates</h1>
+  <StudentCard name="Jerome" language="JavaScript" hobby="Playing Online Games" />
+  <StudentCard name="Rainrev" language="Python" hobby="Sleeping"/>
+  <StudentCard name="Randel" language="Java"  hobby="Drinking Alcohol"/>
+  <StudentCard name="Balong" language="JavaScript"  hobby="Watching movies"/>
+  <StudentCard name="Bernald" language="PHP"  hobby="Playing Mobile Legends"/>
+</template>
+
+<script setup>
+import StudentCard from './components/edwardjohndomingo.vue'
+</script>
