a2936086afebf81a477a816c600b50f8facb25b6
Jhemuel Rayray <jhemuel.rayray1@gmail.com>
2025-09-05 18:12:05 -0700

    Add Vue lab with StudentCard component

---
+++ lab-vue/vue-lab/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>My Classmates</h1>
+  <StudentCard name="Jhemuel Rayray" language="JavaScript" hobby="Playing Badminton" />
+  <StudentCard name="Bernald Catli" language="Python" hobby="Playing Online Games"/>
+  <StudentCard name="Ralph De Guzman" language="Java" hobby="Playing Basketball"/>
+  <StudentCard name="Martin Mercado" language="C++" hobby="Hanging With Friends"/>
+  <StudentCard name="James Hipolito" language="SQL" hobby="Watching Movies"/>
+</template>
+
+<script setup>
+import StudentCard from './components/Jhemuel_Rayray.vue'
+</script>
