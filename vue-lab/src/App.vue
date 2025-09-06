a319c91eee31b089d60f946aa96ea185e3ead73b
Rainrev893 <rainrevcarbonell30@gmail.com>
2025-09-06 08:58:10 +0800

    Add Vue lab with StudentCard component

---
+++ vue-lab/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>My Classmates</h1>
+  <StudentCard name="Rainrev T. Carbonell" language="Java, Dart, and PHP" hobby= "Reading Manga"/>
+  <StudentCard name="Jerome D. Torres" language="Java & Dart " hobby= "Creating Beats"/>
+  <StudentCard name="Timothy James S. Galope" language="Java & Dart" hobby= "Sleeping"/>
+</template>
+
+<script setup>
+import StudentCard from './components/rainrev-carbonell.vue'
+</script>
