bf5a832cf50caf2c889e2f165be738e6e2cd8e3a
Bernald-Catli <bernaldcatli@gmail.com>
2025-09-06 09:43:11 +0800

    Add Vue lab with StudentCard component

---
+++ lab-vue/vue-lab/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>My Classmates</h1>
+  <StudentCard name="Bernald Catli" language="JavaScript" hobby="Gym"/>
+  <StudentCard name="Martin Mercado" language="Python" hobby="Online Games" />
+  <StudentCard name="James Hipolito" language="Java" hobby="Basketball" />
+</template>
+
+<script setup>
+import StudentCard from './components/BernaldCatli.vue'
+</script>
