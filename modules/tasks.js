8e788267f702ef8456a9aaff7eb58bd87f6f26c7
chocoskyyy <mendezdhenniel@gmail.com>
2025-09-13 09:58:45 +0800

    add tasks

---
+++ modules/tasks.js
@@ -0,0 +1,5 @@
+export const teamTasks = [
+  { name: 'Denyel', task: 'tasks', deadline: '10:00' },
+  { name: 'Balong', task: 'grades', deadline: '12:30' },
+  { name: 'Edward', task: 'index', deadline: '2:30' }
+];
