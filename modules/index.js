6f7bdb5c63b6156deb838fcc834cf31687415a67
rainrev <rainrevcarbonell30@gmail.com>
2025-09-13 09:39:17 +0800

    hello

--- modules/index.js
+++ modules/index.js
@@ -1,6 +1,6 @@
 import { add, multiply } from './mathUtils.js';
 import { students } from './students.js';
-
+import { getRandomLeader } from './teams.js';
 
 
 console.log("Add:", add(2, 3));      
@@ -11,3 +11,9 @@ console.log(" ");
 students.forEach(s => {
   console.log(`${s.name} loves ${s.language}`);
 });
+
+console.log(" ");
+
+const leader = getRandomLeader();
+console.log(`${leader.name} is the random leader!`);
+
