35c44927206480bd44d2476a9ce58cfd2a6815c7
unknown <magcalas11082005@gmail.com>
2025-09-13 09:16:38 +0800

    Add tasks and team modules, update index.js

--- Modules/index.js
+++ Modules/index.js
@@ -1,4 +1,4 @@
-import { add, multiply } from './mathUtils.js';
+/*import { add, multiply } from './mathUtils.js';
 import { students } from './students.js';
 
 
@@ -9,4 +9,26 @@ console.log("Multiply:", multiply(4, 2));
 console.log("\nStudent Preferences:");
 students.forEach(s => {
   console.log(`${s.name} loves ${s.language}`);
+});*/
+
+
+// index.js
+
+import { add, multiply } from './mathUtils.js';
+import { students } from './students.js';
+import { getRandomLeader } from './team.js';
+
+// Math operations
+console.log("Add:", add(2, 3));           // Output: 5
+console.log("Multiply:", multiply(4, 2)); // Output: 8
+
+// Student preferences
+console.log("\nStudent Preferences:");
+students.forEach(s => {
+  console.log(`${s.name} loves ${s.language}`);
 });
+
+// Random team leader
+const leader = getRandomLeader();
+console.log(`\nTeam Leader: ${leader.name} (loves ${leader.language})`);
+
