832ed6c1cb26316bfafb4b875f43895e74a21152
ctrljerome <torresjeromed@gmail.com>
2025-09-13 09:57:30 +0800

    feature Jerome

--- modules/index.js
+++ modules/index.js
@@ -1,13 +1,18 @@
 import { add, multiply } from './mathUtils.js';
 import { students } from './students.js';
 import { calculator } from './calculator.js'; import { remarks } from './grade.js';
-
+import { task } from './task.js';
 
 console.log("Add:", add(2, 3));    
 console.log("Multiply:", multiply(4, 2)); 
 
 students.forEach(s => {
   console.log(`${s.name} loves ${s.language}`);
+});
+
+
+task.forEach(s => {
+  console.log(`${s.task}, Deadline ${s.deadline}`);
 });
 
 console.log("2 + 3 =", calculator(2, 3, '+'));
---
+++ modules/task.js
@@ -0,0 +1,5 @@
+export const task = [
+  { task: "Reviewing budgets", deadline: "12:00 PM" },
+  { task: "Managing a project", deadline: "12:00 PM" },
+  { task: "Respond to email clients", deadline: "12:00 PM" }
+];
