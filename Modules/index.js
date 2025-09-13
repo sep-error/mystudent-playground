9898c3c04f82a714996783ce32b3961f1210a332
unknown <magcalas11082005@gmail.com>
2025-09-13 08:44:31 +0800

    JavaScript

---
+++ Modules/index.js
@@ -0,0 +1,12 @@
+import { add, multiply } from './mathUtils.js';
+import { students } from './students.js';
+
+
+console.log("Add:", add(2, 3));         
+console.log("Multiply:", multiply(4, 2)); 
+
+
+console.log("\nStudent Preferences:");
+students.forEach(s => {
+  console.log(`${s.name} loves ${s.language}`);
+});
