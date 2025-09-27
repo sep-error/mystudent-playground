b8d1e7cc644f1e8c71ed350bfa65c0109ac92b1d
jeromemangune2004 <Jeromemangune2004@gmail.com>
2025-09-27 08:42:07 +0800

    Section 2

---
+++ midterm-js-Jerome-Mangune/index.js
@@ -0,0 +1,7 @@
+// index.js
+import { students } from './students.js';
+import { add, multiply } from './mathUtils.js';
+
+console.log("Add:", add(2, 3));        // Output: Add: 5
+console.log("Multiply:", multiply(4, 2)); // Output: Multiply: 8
+
